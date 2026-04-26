1bbfc18c4940a82a15b8c601c27f3e683159e380
bingh <bhuang@msudenver.edu>
2026-04-25 19:01:50 -0600

    cors fixed + moved imports

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import emailRouter from './utils/email.js';
+import careersRouter from './routes/careers.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}));
 app.use(express.json());
 app.use('/api', emailRouter);
+app.use('/api', careersRouter);
 
-// console.log("API KEY:", process.env.SENDGRID_API_KEY);
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
