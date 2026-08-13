db42a4d7c0ee4aa6f6dc4c2035c53899dc3e0646
Owen Pick <mercuryman1220@gmail.com>
2026-08-13 11:29:04 -0600

    pre-commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HollandQuiz from './components/HollandQuiz';
 import Welcome from "./components/Welcome";
-import FooterBar from "./components/FooterBar";
 import './App.css';
 
 function App() {
@@ -20,11 +19,6 @@ function App() {
             <Route path="/quiz" element={<HollandQuiz />} />
           </Routes>
         </main>
-
-        <FooterBar
-          denverUrl="https://map.concept3d.com/?id=225#!ct/95756,95757,95758?s/"
-          phoneText="303-556-5740"
-        />
       </div>
 
     </Router>
